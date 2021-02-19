2b3bfe3d49c159f5f26ab55a8ad2f27f84589362
Amir B <amir.boutayeb@gmail.com>
2021-02-19 01:00:31 +0100

    DAG for dbt-dremio

--- dags/generate_dbt_source.py
+++ dags/generate_dbt_source.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from datetime import datetime
 import pandas as pd
 import requests as rq
@@ -21,59 +22,68 @@ from datafuel.minio_fuel import (get_minio_client, df_to_csv_inMinio,
 default_args = {
     'owner': 'ABO',
 }
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['datalake'])
+@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['dbt'])
 def generate_dbt_source(
-    DREMIO_DRIVER: str = "Dremio ODBC Driver 64-bit",
+    # DREMIO_DRIVER: str = "Dremio ODBC Driver 64-bit",
+    DREMIO_DRIVER: str = "/opt/dremio-odbc/lib64/libdrillodbc_sb64.so",
     DREMIO_HOST: str = "dremio",
-    DREMIO_PORT: int = 9047,
+    DREMIO_PORT: str = "31010",
     DREMIO_ENVIRONMENT: str = "prd",
     DREMIO_DATABASE: str = "datalake-bucket",
     DREMIO_SCHEMA: str = "stg",
     DREMIO_USER: str = "amirb",
     DREMIO_PASSWORD: str = "pass4dremio",
     PROJECT_GITHUB_REPO: str = "https://github.com/datafuel/sirene-06_dbt.git",
-    PROJECT_REPO: str = "sirene-06_dbt",
+    PROJECT_REPO: str = "sirene-06_dbt"
 ):
     """
-    ### TaskFlow API Tutorial Documentation
-    This is a simple ETL data pipeline example which demonstrates the use of
-    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
-    Documentation that goes along with the Airflow TaskFlow API tutorial is
-    located
-    [here](https://airflow.apache.org/docs/stable/tutorial_taskflow_api.html)
+    ### DBT Test
     """
     
-    # @task()
-    # def log_datalake(schema): 
-    #     # a = run_SQL(f'SHOW TABLES FROM hive.{schema}')
-    #     engine = get_engine()
-    #     query_result = run_SQL(command=f'SHOW TABLES FROM hive.{schema}', engine=engine)
-    #     logging.info('Tables in Presto : ')
-    #     logging.info(query_result.fetchall())
 
-    
-    
-    dbt_clone = BashOperator(
-        task_id='dbt_clone',
-        bash_command='mkdir /dbt && cd /dbt && git clone https://github.com/datafuel/sirene-06_dbt.git',
-        dag=dag
+    clean_dbt_project = BashOperator(
+        task_id='clean_dbt_project',
+        bash_command='rm -rf ~/home/dbt'
     )
 
-    # dbt_deps = BashOperator(
-    #     task_id='dbt_deps',
-    #     bash_command=f'cd /dbt/{PROJECT_REPO} && dbt deps',
-    #     dag=dag
-    # )
+    clone_command = """
+    mkdir -p ~/home/dbt \
+        && cd ~/home/dbt \
+        && git clone {{ dag_run.conf['PROJECT_GITHUB_REPO'] }} \
+        && pwd \
+        && ls
+    """
+    dbt_clone = BashOperator(
+        task_id='dbt_clone',
+        bash_command=clone_command
+    )
 
+
+    debug_command = """
+    cd ~/home/dbt/sirene-06_dbt \
+        && ls \
+        && dbt debug --profiles-dir profiles
+    """
+    # debug_command="cd ~/home/dbt/sirene-06_dbt && ls"
     dbt_debug = BashOperator(
         task_id='dbt_deps',
-        bash_command='cd /dbt/sirene-06_dbt && ls -l',
-        dag=dag
+        bash_command=debug_command,
+        env={
+            "DREMIO_DRIVER":DREMIO_DRIVER,
+            "DREMIO_HOST":DREMIO_HOST,
+            "DREMIO_PORT":DREMIO_PORT,
+            "DREMIO_ENVIRONMENT":DREMIO_ENVIRONMENT,
+            "DREMIO_DATABASE":DREMIO_DATABASE,
+            "DREMIO_SCHEMA":DREMIO_SCHEMA,
+            "DREMIO_USER":DREMIO_USER,
+            "DREMIO_PASSWORD":DREMIO_PASSWORD
+        }
     )    
 
 # dbt_deps.set_upstream(dbt_clone)
         
-    dbt_clone >> dbt_debug
+    clean_dbt_project >> dbt_clone >> dbt_debug
+    # generate_dbt_vars >> dbt_debug
     
     
 generate_dbt_source_dag = generate_dbt_source()
